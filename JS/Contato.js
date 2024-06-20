b4167c57aa4e59030d811af4882c8214f3e999c6
MURILO ALMEIDA DE OLIVEIRA <murilo.aoliveira11@senacsp.edu.br>
2024-06-20 20:20:22 -0300

    Primeiro envio

---
+++ JS/Contato.js
@@ -0,0 +1,16 @@
+var enviar = document.getElementById("enviar")
+var nome = document.getElementById("nome").value
+var masc = document.getElementById("masc")
+var fem = document.getElementById("fem")
+var email = document.getElementById("email")
+var estado = document.getElementById("estado")
+var comentario = document.getElementById("comentario")
+
+enviar.addEventListener("click", function (){
+
+    checaForm.addEventListener("click", function (){
+        
+    })
+
+
+})
